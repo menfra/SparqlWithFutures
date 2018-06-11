eba07f12ac7b032f914fd2558e3faacb1ca54e2d
Frank Mensah <menfra.osd@gmail.com>
2018-06-11 04:45:16 +0200

    Modified

--- eclipse-workspace/sparqlQuery/src/main/java/sparqlQuery/sparqlClass.java
+++ eclipse-workspace/sparqlQuery/src/main/java/sparqlQuery/sparqlClass.java
@@ -15,28 +15,30 @@ import java.util.concurrent.Future;
 
 public class sparqlClass {
 	
-	private static String sparqlQuery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" + 
-			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
-			"PREFIX dbo: <http://dbpedia.org/ontology/>\r\n" + 
-			"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n" + 
-			"SELECT DISTINCT ?resc WHERE {\r\n" + 
-			"  {?resc rdf:type dbo:Place .\r\n" + 
-			"  ?resc rdfs:label ?labelDe .\r\n" + 
-			"  ?resc foaf:name ?labelEn . \r\n" + 
-			"    Filter (lang(?labelDe) = 'de') .}\r\n" + 
-			"  UNION\r\n" + 
-			"  {?resc rdf:type dbo:Place .\r\n" + 
-			"   ?resc rdfs:label ?labelDe .\r\n" + 
-			"  ?resc foaf:name ?labelEn .\r\n" + 
-			"    Filter (lang(?labelDe) = 'en') .}\r\n" + 
-			"} \r\n" + 
-			"LIMIT 500";
-	private static String path = "result.txt";
-	
+	private String sparqlQuery;
+	private String path;
 	public sparqlClass() {
+		this.path = "result.txt";
+		this.sparqlQuery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n" + 
+				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
+				"PREFIX dbo: <http://dbpedia.org/ontology/>\r\n" + 
+				"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n" + 
+				"PREFIX owl: <http://www.w3.org/2002/07/owl#>\r\n" + 
+				"SELECT DISTINCT * WHERE {\r\n" + 
+				"  {?resc rdf:type dbo:Place .\r\n" + 
+				"  ?resc rdfs:label ?labelDe .\r\n" + 
+				"  ?resc foaf:name ?labelEn . \r\n" + 
+				"    Filter (lang(?labelDe) = 'de') .}\r\n" + 
+				" UNION\r\n" + 
+				"  {?resc rdf:type dbo:Place .\r\n" + 
+				"   ?resc rdfs:label ?labelDe.\r\n" + 
+				"  ?resc foaf:name ?labelEn .\r\n" + 
+				"    Filter (lang(?labelEn) = 'en') .}\r\n" + 
+				" \r\n" + 
+				"} \r\n" + 
+				"LIMIT 500";
 	}
-	
-	
+
 	public static void writeFile(String writeMessage, String path) 
 			  throws IOException {
 		        File file = new File(path);
@@ -46,22 +48,32 @@ public class sparqlClass {
 			     
 			    writer.close();
 			}
+	
+	//Fibonacci Implementation 
+	 public static String fibonacci(int n) {
+	        if (n <= 1) return Integer.toString(n);
+	        else return fibonacci(n-1) + fibonacci(n-2);
+	    }
 
+	 
 	    public static void main(String[] args) {
 	    	ExecutorService executor = Executors.newCachedThreadPool();
 	    	
-	    	ParameterizedSparqlString qs = new ParameterizedSparqlString(sparqlQuery);
-	        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
-	        final ResultSet results = exec.execSelect();
-	        
+	    	ParameterizedSparqlString qs = new ParameterizedSparqlString((new sparqlClass()).sparqlQuery);
 	    		
 	    	Future<String> future = executor.submit(new Callable<String>() {
 	    		
 	    		public String call() throws Exception{
+	    			
 	    	        
 	    	        System.out.println("Starting Thread...");
+	    	        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
+	    	        final ResultSet results = exec.execSelect();
+	    	        
+	    	        //fibonacci(30);
 	    	        while (results.hasNext()) {
-					writeFile(results.next().toString(),path);
+					//writeFile(results.next().toString(),(new sparqlClass()).path);
+	    	          writeFile(fibonacci(5),(new sparqlClass()).path);
 					}
 
 	    	        System.out.println("Ended Thread...");
